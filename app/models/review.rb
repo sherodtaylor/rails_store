d488ed8fa69ea06ee7d51337d1987163a4252cbc
Sherod Taylor <sherodtaylor@gmail.com>
2013-04-15 09:11:40 -0700

    more styling

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ActiveRecord::Base
   attr_accessible :body, :product_id, :rating
   belongs_to :product
-  validates_associated :body, :length => { :in => 50..1000 }
-  validates :rating, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5 }
-
+  validates :body, :length => { :in => 50..1000 }
+  validates :rating, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 5 }
 end
